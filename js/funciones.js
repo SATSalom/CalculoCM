d0279b983c7ab38f175058f840028bb9dd616c55
Brian Passos <bri@npass.us>
2022-02-07 16:17:59 +0100

    Lógica refactorizada, añadida línea para debug de valores en consola.

--- js/funciones.js
+++ js/funciones.js
@@ -1,30 +1,28 @@
 'use strict';
 
 const el = {
-	formulario: document.body.querySelector ('form[name="calculoCM"]'), // Elemento del formulario principal.
 	inputE: document.body.querySelectorAll ('input:not([readonly])'), // Elementos input editables.
 	inputN: document.body.querySelectorAll ('input[readonly]') // Elementos input sólo legible.
 },
 valores = () => { // Función que obtiene los valores, procesa los cálculos necesarios y muestra los resultados en los campos pertinentes.
-	const A = el.inputE[0].value,
-		B = el.inputE[1].value,
-		C = Number (A*B)+Number(A),
-		D = el.inputE[2].value,
-		E = Number (C*D) + Number (C)
+	const A = Number (el.inputE[0].value),
+	B = Number (el.inputE[1].value),
+	C = Number (A*B) + Number(A),
+	D = Number (el.inputE[2].value),
+	E = Number (C*D) + Number (C);
 
-	el.inputN[0].value = C; // Muestra el resultado en input C
-	el.inputN[1].value = parseFloat(E).toFixed(3); // Muestra resultado en input E, redondeando al tercer decimal.
+	el.inputN[0].value = parseFloat(C).toFixed(4); // Muestra el resultado en input C, redondeando al cuarto decimal por defecto.
+	el.inputN[1].value = parseFloat(E).toFixed(4); // Muestra resultado en input E, redondeando al cuarto decimal por defecto.
+
+	return {A,B,C,D,E}; // Para debug.
 },
 registrarEventos = () => { // Asigna un evento que ejecuta código cada vez que un valor cambie en cualquiera de los input editables.
 	el.inputE.forEach (el => el.addEventListener ('change', () => {
-		calcularValores ();
+		calcularValores (); // Llama a la función que engloba a las demás necesarias para los cálculos.
 	}));
 },
 calcularValores = () => {
-	valores ()
-	console.info("test")
+	console.info (valores ()); // Calculamos y mostramos en consola valores sin redondear.
 };
-
-
 
 registrarEventos (); // Llamamos a la función para que los eventos tengas efecto.
